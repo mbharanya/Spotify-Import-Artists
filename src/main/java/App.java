db11650ec43e5b0a31b2355818a0e878bb50a364
XMBomb <xmbomb@users.noreply.github.com>
2018-03-08 16:39:04 +0100

    Upgrade Spotify Library to 2.0.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,12 @@
-import com.wrapper.spotify.Api;
-import com.wrapper.spotify.exceptions.WebApiException;
-import com.wrapper.spotify.methods.AlbumRequest;
-import com.wrapper.spotify.methods.ArtistRequest;
-import com.wrapper.spotify.methods.ArtistSearchRequest;
-import com.wrapper.spotify.models.Album;
-import com.wrapper.spotify.models.Artist;
-import com.wrapper.spotify.models.Track;
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Artist;
+import com.wrapper.spotify.requests.data.search.simplified.SearchArtistsRequest;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,9 +18,9 @@ public class App {
     public static final int API_WAIT_TIME = 100;
     private static final String ACCESS_TOKEN_PATH = "res"+File.separator+"auth-key.txt";
 
-    public static void main(String... args) throws IOException, WebApiException, InterruptedException {
+    public static void main(String... args) throws IOException{
         // Create an API instance. The default instance connects to https://api.spotify.com/.
-        Api api = Api.builder().accessToken(FileUtils.readFileToString(new File(ACCESS_TOKEN_PATH))).build();
+        SpotifyApi api = new SpotifyApi.Builder().setAccessToken(FileUtils.readFileToString(new File(ACCESS_TOKEN_PATH))).build();
 
         List<String> artistNames = FileUtils.readLines(new File(ARTISTS_FILE_PATH), "UTF-8");
 
@@ -34,7 +28,7 @@ public class App {
 
         artistNames.stream()
                 .forEach(artistName -> {
-                    ArtistSearchRequest request = api.searchArtists(artistName).build();
+                    SearchArtistsRequest request = api.searchArtists(artistName).build();
                     try {
                         Thread.sleep(API_WAIT_TIME);
                     } catch (InterruptedException e) {
@@ -42,18 +36,18 @@ public class App {
                         e.printStackTrace();
                     }
                     try {
-                        final List<Artist> artists = request.get().getItems();
-                        if (artists.size() <= 0){
+                        Artist[] artists = request.execute().getItems();
+                        if (artists.length <= 0){
                             System.err.println("No Artist found for artistName "+artistName);
                             return;
                         }
-                        final Artist artist = artists.get(0);
+                        final Artist artist = artists[0];
                         System.out.println("Working on "+artist.getName());
                         artistIds.add(artist.getId());
                     } catch (IOException e) {
                         System.err.println(e);
                         e.printStackTrace();
-                    } catch (WebApiException e) {
+                    } catch (SpotifyWebApiException e) {
                         System.err.println(e);
                         e.printStackTrace();
                     }
